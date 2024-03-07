649f0727644ee9a2d549b4f765bcade231de67db
Kaleb Teshale <kalebteshale72@gmail.com>
2024-03-07 12:11:19 +0300

    backend database conncetion done

---
+++ ffdb-backend/src/database/database.provider.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize-typescript';
+
+export const databaseProviders = [
+    {
+        provide: 'SEQUELIZE',
+        useFactory: async () => {
+            const sequelize = new Sequelize({
+                dialect: 'postgres',
+                host: process.env.DATABASE_HOST,
+                port: parseInt(process.env.DATABASE_PORT),
+                username: process.env.DATABASE_USER,
+                password: process.env.DATABASE_PASSWORD,
+                database: 'mms',
+                ssl: true,
+                dialectOptions: {
+                    ssl: true
+                }
+            });
+            sequelize.addModels([]);
+            await sequelize.sync({ alter: true });
+            return sequelize;
+        },
+    },
+];
